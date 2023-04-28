04ad705b9a7db1027ab93cecb2732042901daec8
Bilal Ali <bilalal495@outlook.com>
2023-04-28 02:21:05 -0400

    Add unit test for computeNauticalMiles method

--- tests/CellNetworkTest.java
+++ tests/CellNetworkTest.java
@@ -2,12 +2,18 @@
  * This class is used to test the functionality of the CellNetwork class
  */
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CellNetworkTest {
 
     @Test
     public void testComputeNauticalMiles() {
-
+        double lat1 = 39.38;
+        double long1 = 74.45;
+        double lat2 = 39.58;
+        double long2 = 74.87;
+        int distance = ICellNetwork.computeNauticalMiles(lat1, long1, lat2, long2);
+        Assert.assertEquals(23, distance);
     }
 }
