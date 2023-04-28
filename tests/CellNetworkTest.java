7f97c5bd6aedcd0867039246ce35f88c2428a48f
Bilal Ali <bilalal495@outlook.com>
2023-04-28 01:17:09 -0400

    Add class to unit test CellNetwork class

---
+++ tests/CellNetworkTest.java
@@ -0,0 +1,13 @@
+/**
+ * This class is used to test the functionality of the CellNetwork class
+ */
+
+import org.junit.Test;
+
+public class CellNetworkTest {
+
+    @Test
+    public void testComputeNauticalMiles() {
+
+    }
+}
