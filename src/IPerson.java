b53ccbdf2d53d5da8bc2534d0c61f38fdd3d9238
aashok12 <aashok@seas.upenn.edu>
2023-04-29 00:31:36 -0400

    Initial ctors and tests for Person done.
    Need to create QuadTree class to implement non ctor methods

--- src/IPerson.java
+++ src/IPerson.java
@@ -1,3 +1,7 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This interface defines the behavior of a person making a call through a cell phone network.
  * Implementing classes must provide functionality to determine the cell tower closest to the
@@ -5,5 +9,30 @@
  */
 
 public interface IPerson {
-
+    /**
+     * This map is used internally to map different cell providers to AT&T or Verizon based on business relationships
+     * These can be used to decide the premium that users will pay for celltower usage
+     */
+    @SuppressWarnings("serial")
+    public static final Map<String, String> PROVIDERMAP =
+            Collections.unmodifiableMap(new HashMap<String, String>()
+            {
+            {
+            put("at&t", "AT&T");
+            put("verizon", "Verizon");
+            put("cricket", "AT&T");
+            put("h20", "AT&T");
+            put("tmobile", "AT&T");
+            put("t-mobile", "AT&T");
+            put("xfinity", "Verizon");
+            
+            }
+            }); 
+    
+    
+   // boolean hasCoverage(QuadTree qt);
+    
+   // int closestTower(QuadTree qt);
+    
+    
 }
--- src/Person.java
+++ src/Person.java
@@ -3,4 +3,57 @@
  */
 
 public class Person implements IPerson {
+    
+    private String name;
+    private double longitude;
+    private double latitude;
+    private String carrier;
+    
+    
+    public Person(String firstName, String cellCarrier, double lat, double lon) {
+        // TODO Auto-generated constructor stub
+        setName(firstName);
+        setCarrier(cellCarrier);
+        setLong(lon);
+        setLat(lat);
+        
+    }
+
+    public double getLong() {
+        return longitude;
+    }
+
+    public void setLong(double lon) {
+        this.longitude = lon;
+    }
+
+    public double getLat() {
+        return latitude;
+    }
+
+    public void setLat(double lat) {
+        this.latitude = lat;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        if (PROVIDERMAP.containsKey(carrier)) {
+            carrier = carrier.toLowerCase();
+            this.carrier = PROVIDERMAP.get(carrier);
+        } else {
+            this.carrier = carrier;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
