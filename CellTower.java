2b2aeaf9bc820a02792e0222e6fe383ad996ff2f
swapnilnlal <swapnilnlal@gmail.com>
2023-04-30 13:45:17 -0400

    Add files via upload
    Made changes to CellTower, Process their respective interfaces and wrote test cases.

---
+++ CellTower.java
@@ -0,0 +1,136 @@
+/**
+ * This class provides a concrete implementation of the ICellTower interface.
+ */
+
+public class CellTower implements ICellTower {
+    
+    private String license;
+    private int id;
+    private String address;
+    private String city;
+    private String county;
+    private String state;
+    private double latitude;
+    private double longitude;
+    
+    
+    /**
+     * ctor
+     */
+    public CellTower() {
+        setId(0);
+        setLicense(null);
+        setAddress(null);
+        
+        setCity(null);
+        setCounty(null);
+        setLat(0);
+        
+        setLong(0);
+        setState(null);
+        
+    }
+    
+    public CellTower(int towerID, double latDeg, double longDeg, String licensee, String ad, String cty, String county, 
+            String st) {
+       
+        setId(towerID);
+        setLicense(licensee);
+        setAddress(ad);
+        setCity(cty);
+        setCounty(county);
+        setLat(latDeg);
+        setLong(longDeg);
+        setState(st);
+    }
+
+
+    public String getLicense() {
+        return license;
+    }
+
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public String getCity() {
+        return city;
+    }
+
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+    public String getCounty() {
+        return county;
+    }
+
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+
+    public String getState() {
+        return state;
+    }
+
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+    public double getLat() {
+        return latitude;
+    }
+
+
+    public void setLat(double latitude) {
+        this.latitude = latitude;
+    }
+
+
+    public double getLong() {
+        return longitude;
+    }
+
+
+    public void setLong(double longitude) {
+        this.longitude = longitude;
+    }
+    
+    @Override
+    public String toString() {
+        String out = null;
+        
+        out =  id + ": " + county +"," + city + ", " + state + " located at: (" + latitude + ","  + longitude + ")" + "\n";
+        
+        return out;
+        
+    }
+}
