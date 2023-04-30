9a7c380e199e2bd8fafe5fe8e298f196a541b3f6
aashok12 <aashok@seas.upenn.edu>
2023-04-30 13:46:39 -0400

    cell tower test fixed

--- tests/CellTowerTest.java
+++ tests/CellTowerTest.java
@@ -4,8 +4,8 @@ public class CellTowerTest {
 
     @Test
     public void toStringTest() {
-        CellTower ct = new CellTower("a", 12, "123 Main St", "Nowhere","None", "NW", 1.006324, 45.68374);
         
+        CellTower ct = new CellTower(12, 1.006324, 45.68374, "AT&T", "123 Main St", "Nowhere","None", "NW");
         System.out.println(ct);
         
     }
