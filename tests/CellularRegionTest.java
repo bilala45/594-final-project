bdb1afbdcdec2202263337d1639a5338bc347153
Bilal Ali <bilalal495@outlook.com>
2023-05-01 17:01:44 -0400

    Fix cellular region tests

--- tests/CellularRegionTest.java
+++ tests/CellularRegionTest.java
@@ -15,14 +15,13 @@ public class CellularRegionTest {
         cr = new CellularRegion(new Coordinate(50, -85) , new Coordinate(34, -69), null);
         List<CellTower> cellTowers = new ArrayList<>();
         //Name, id etc. of towers do not matter.
-        cellTowers.add(new CellTower(1, 41.34977778, -72.97266667, "a", "b", "c", "d", "e"));
-        cellTowers.add(new CellTower(2, 41.57036111, -73.01788889, "a", "b", "c", "d", "e"));
-        cellTowers.add(new CellTower(3, 41.45066667, -73.516, "a", "b", "c", "d", "e"));
-        cellTowers.add(new CellTower(4, 41.522, -73.22075, "a", "b", "c", "d", "e"));
-        cellTowers.add(new CellTower(5, 41.03394444, -73.63083333, "a", "b", "c", "d", "e"));
-        cellTowers.add(new CellTower(6, 41.27911111, -73.18525, "a", "b", "c", "d", "e"));
+        cellTowers.add(new CellTower(1, 41.34977778, -72.97266667, "verizon", "b", "c", "d", "e"));
+        cellTowers.add(new CellTower(2, 41.57036111, -73.01788889, "verizon", "b", "c", "d", "e"));
+        cellTowers.add(new CellTower(3, 41.45066667, -73.516, "verizon", "b", "c", "d", "e"));
+        cellTowers.add(new CellTower(4, 41.522, -73.22075, "verizon", "b", "c", "d", "e"));
+        cellTowers.add(new CellTower(5, 41.03394444, -73.63083333, "verizon", "b", "c", "d", "e"));
+        cellTowers.add(new CellTower(6, 41.27911111, -73.18525, "verizon", "b", "c", "d", "e"));
         cr.setTowersInRegion(cellTowers);
-        
     }
 
 
@@ -63,28 +62,23 @@ public class CellularRegionTest {
     public void TopLeftTest() {
         assertEquals(cr.topLeft.getLatitude(), 50, 0.01);
         assertEquals(cr.topLeft.getLongitude(), -85, 0.01);
- 
     }
     
     @Test
-    public void TestChildern() {
+    public void TestChildren() {
         assertNull(cr.getTopLeftSq());
         assertNull(cr.getTopRightSq());
         assertNull(cr.getBotRightSq());
         assertNull(cr.getBotLeftSq());
-
     }
-    
+
+
     @Test
     public void TestSiblings() {
         cr.subDivide();
-        assertNull(cr.getTopLeftSq());
-        assertNull(cr.getTopRightSq());
-        assertNull(cr.getBotRightSq());
-        assertNull(cr.getBotLeftSq());
-
+        assertNotNull(cr.getTopLeftSq());
+        assertNotNull(cr.getTopRightSq());
+        assertNotNull(cr.getBotRightSq());
+        assertNotNull(cr.getBotLeftSq());
     }
-    
-
-
 }
